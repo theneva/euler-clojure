e23cd348a0dd14986fa76af7fcbdedbb355e4068
Martin Lehmann <martin@lehmann.tech>
2017-02-26 20:44:19 +0100

    Fix 04 to only look for products of 3 digit numbers

--- 04.clj
+++ 04.clj
@@ -2,8 +2,8 @@
   (let [s (str n)]
     (= s (clojure.string/reverse s))))
 
-(->> (for [x (range 10 1000)
-           y (range 10 1000)]
+(->> (for [x (range 100 1000)
+           y (range 100 1000)]
        (* x y))
      (filter palindromic?)
      (reduce max))
